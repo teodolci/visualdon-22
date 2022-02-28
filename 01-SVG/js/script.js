a4f17ca5573bde08860d54bbcca094b9961fe582
Teo Dolci <teo.dolci@heig-vd.ch>
2022-02-28 08:41:58 +0100

    modif donut:onHover

--- 01-SVG/js/script.js
+++ 01-SVG/js/script.js
@@ -1,4 +1,5 @@
 let rectangle = document.getElementById("rectOnClick");
+let donut = document.getElementById("donut");
 
 rectangle.onclick = function(){
     if (rectangle.style.fill == "blue") {
@@ -7,3 +8,18 @@ rectangle.onclick = function(){
         rectangle.style.fill = "blue";
     }
 };
+
+donut.addEventListener('mouseenter', evt => {
+    donut.querySelector(".extDonut").setAttribute("r", 70);
+})
+
+donut.addEventListener('mouseleave', evt => {
+    donut.querySelector(".extDonut").setAttribute("r", 60);
+})
+
+// donut.addEventListener('mouseover', evt => {
+//         evt = evt.target;
+//         console.log(evt);
+//         /* donut.setAttribute("r","100") */
+//         donut.classList.toggle('changerayon')
+// })
