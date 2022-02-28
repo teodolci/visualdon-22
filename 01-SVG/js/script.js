fcb5f4abaf6a0b4a9303fa231f438983136446d7
Teo Dolci <teo.dolci@heig-vd.ch>
2022-02-28 12:27:54 +0100

    optimisation :hover

--- 01-SVG/js/script.js
+++ 01-SVG/js/script.js
@@ -1,5 +1,4 @@
 let rectangle = document.getElementById("rectOnClick");
-let donut = document.getElementById("donut");
 
 rectangle.onclick = function(){
     if (rectangle.style.fill == "blue") {
@@ -8,18 +7,3 @@ rectangle.onclick = function(){
         rectangle.style.fill = "blue";
     }
 };
-
-donut.addEventListener('mouseenter', evt => {
-    donut.querySelector(".extDonut").setAttribute("r", 70);
-})
-
-donut.addEventListener('mouseleave', evt => {
-    donut.querySelector(".extDonut").setAttribute("r", 60);
-})
-
-// donut.addEventListener('mouseover', evt => {
-//         evt = evt.target;
-//         console.log(evt);
-//         /* donut.setAttribute("r","100") */
-//         donut.classList.toggle('changerayon')
-// })
