ff80fde80234d2b6b6f62fd07efb7b0ba492df13
Teo Dolci <teo.dolci@heig-vd.ch>
2022-04-08 15:53:32 +0200

    Exerice 2 gapminder

--- 04-gapminder/src/index.js
+++ 04-gapminder/src/index.js
@@ -1,5 +1,6 @@
 import * as d3 from 'd3'
 import { timeInterval } from 'd3'
+import { count } from 'd3';
 
 // Pour importer les données
 import gdp from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
@@ -137,11 +138,88 @@ import population from '../data/population_total.csv'
 // **************************************
 // EXERCICE 2
 
+let listCountries = []
 
+lifeExpectancy.forEach(row => {
+    let countryData = {};
+    countryData[row['country']] = row['2021']
+    listCountries.push(countryData)
+});
+console.log(listCountries);
+
+d3.select("body")
+    .append("div")
+    .attr('id', 'graph')
+
+let margin = { top: 20, right: 20, bottom: 30, left: 50 },
+    width = 650 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+let svg = d3.select("#graph")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+
+let projection = d3.geoMercator()
+    .scale(70)
+    .center([0, 20])
+    .translate([width / 2, height / 2]);
+
+let aRandomNb = Math.floor(Math.random() * 6);
+let aRandomScheme;
+switch (aRandomNb) {
+    case 0:
+        aRandomScheme = d3.schemeOranges;
+        break;
+    case 1:
+        aRandomScheme = d3.schemeGreens;
+        break;
+    case 2:
+        aRandomScheme = d3.schemeReds;
+        break;
+    case 3:
+        aRandomScheme = d3.schemeBlues;
+        break;
+    case 4:
+        aRandomScheme = d3.schemeGreys;
+        break;
+    case 5:
+        aRandomScheme = d3.schemePurples;
+        break;
+}
+
+
+let colorScale = d3.scaleThreshold()
+    .domain([50, 60, 70, 80, 90, 100])
+    .range(aRandomScheme[7]);
+
+d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then(function (d) {
+    // Draw the map
+    svg.append("g")
+        .selectAll("path")
+        .data(d.features)
+        .join("path")
+        // draw each country
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        // set id
+        .attr("id", function (d) { return d.properties.name; })
+        .attr("fill", function (d) {
+            let number = 0;
+            listCountries.forEach(country => {
+                if (typeof country[this.id] != "undefined") {
+                    console.log(country[this.id]);
+                    number = country[this.id]
+                }
+            })
+            console.log(number);
+            return colorScale(number);
+        })
+})
 
 // **************************************
 // EXERCICE 3
 // import gdp from '../data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv'
 // import lifeExpectancy from '../data/life_expectancy_years.csv'
 // import population from '../data/population_total.csv'
-
